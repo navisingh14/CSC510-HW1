b84e6c3424544e8673c3b64a87e45c766698c626
nsingh9 <nsingh9@ncsu.edu>
2017-09-06 16:45:27 -0700

    adding script.js

---
+++ script.js
@@ -0,0 +1,215 @@
+var request = require('request');
+var fs = require("fs");
+var Promise = require('bluebird');
+var parse = require('parse-link-header');
+
+
+////// FILL IN THE BLANKS
+
+var token = "token " + "<Your Token>";
+var userId = "navisingh14";
+
+var urlRoot = "https://api.github.com";
+// NCSU Enterprise endpoint:
+// https://github.ncsu.edu/api/v3
+
+var repo = "REST-SELENIUM";
+var newRepoName = "CSC510-HW1"; 
+var number = '1';
+
+//getYourRepos(userId);
+//listBranches(userId, repo);
+//createNewRepo(repoName);
+//createNewIssue(userId, repo);
+//editRepo(userId, repo);
+listReactions(userId,repo, number);
+
+function getYourRepos(userName)
+{
+
+	var options = {
+		url: urlRoot + '/users/' + userName + "/repos",
+		method: 'GET',
+		headers: {
+			"User-Agent": "EnableIssues",
+			"content-type": "application/json",
+			"Authorization": token
+		}
+	};
+
+	// Send a http request to url and specify a callback that will be called upon its return.
+	request(options, function (error, response, body) 
+	{
+		var obj = JSON.parse(body);
+		console.log( obj );
+		for( var i = 0; i < obj.length; i++ )
+		{
+			var name = obj[i].name;
+			console.log( name );
+		}
+	});
+
+}
+
+function listBranches(owner,repo)
+{
+
+	var options = {
+		url: urlRoot + '/repos/' + owner + "/" + repo + "/branches",
+		method: 'GET',
+		headers: {
+			"User-Agent": "EnableIssues",
+			"content-type": "application/json",
+			"Authorization": token
+		}
+	};
+
+	// Send a http request to url and specify a callback that will be called upon its return.
+	request(options, function (error, response, body) 
+	{
+		var obj = JSON.parse(body);
+		console.log( obj );
+		for( var i = 0; i < obj.length; i++ )
+		{
+			var name = obj[i].name;
+			console.log( name );
+		}
+	});
+
+	
+}
+
+function createNewRepo(newRepoName)
+{
+
+	var options = {
+		url: urlRoot + '/user/repos',
+		method: 'POST',
+		headers: {
+			"User-Agent": "EnableIssues",
+			"content-type": "application/json",
+			"Authorization": token
+		},
+		json: {
+			"name": newRepoName,
+			"description": "This is your first repository",
+			"homepage": "https://github.com",
+			"private": false,
+			"has_issues": true,
+			"has_projects": true,
+			"has_wiki": true
+		  }
+
+
+	};
+
+	// Send a http request to url and specify a callback that will be called upon its return.
+	request(options, function (error, response, body) 
+	{
+//		console.log( body );
+		console.log( response.statusCode );
+	});
+
+	
+}
+
+
+function createNewIssue(owner, repo)
+{
+
+	var options = {
+		url: urlRoot + '/repos/' + owner + "/" + repo + "/issues",
+		method: 'POST',
+		headers: {
+			"User-Agent": "EnableIssues",
+			"content-type": "application/json",
+			"Authorization": token
+		},
+		json: {
+			"title": "Found an issue",
+			"body": "I'm having a problem with this.",
+	//		"assignee": "navisingh14",
+	//		"milestone": 1,
+	//		"labels": [
+	//		  "bug"
+	//		]
+		  }
+
+
+	};
+
+	// Send a http request to url and specify a callback that will be called upon its return.
+	request(options, function (error, response, body) 
+	{
+//		console.log( body );
+		console.log( response.statusCode );
+		console.log( response );
+		
+	});
+
+	
+}
+
+
+function editRepo(owner, repo)
+{
+
+	var options = {
+		url: urlRoot + '/repos/' + owner + '/' + repo,
+		method: 'PATCH',
+		headers: {
+			"User-Agent": "EnableIssues",
+			"content-type": "application/json",
+			"Authorization": token
+		},
+		json: {
+			"name": repo,
+			"description": "CSC510-HW1",
+			"homepage": "https://github.com",
+			"private": false,
+			"has_issues": true,
+			"has_projects": true,
+			"has_wiki": false
+		  }
+
+
+	};
+
+	// Send a http request to url and specify a callback that will be called upon its return.
+	request(options, function (error, response, body) 
+	{
+//		console.log( body );
+		console.log( response.statusCode );
+//		console.log(response);
+	});
+}
+
+
+function listReactions(owner,repo, number)
+{
+
+	var options = {
+		url: urlRoot + '/repos/' + owner + "/" + repo + "/issues/" + number + "/reactions" ,
+		method: 'GET',
+		headers: {
+			"User-Agent": "EnableIssues",
+			"content-type": "application/json",
+			"Authorization": token,
+			"Accept" : "application/vnd.github.squirrel-girl-preview"
+		}
+	};
+
+	// Send a http request to url and specify a callback that will be called upon its return.
+	request(options, function (error, response, body) 
+	{
+		var obj = JSON.parse(body);
+		console.log( obj );
+		for( var i = 0; i < obj.length; i++ )
+		{
+			var name = obj[i].name;
+			console.log( name );
+		}
+	});
+
+	
+}
